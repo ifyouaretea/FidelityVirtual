46dc5fc6cce13c1c0888522339032f26fa13b55c
ifyouaretea <furtado1411@gmail.com>
2016-04-13 17:22:57 +0800

    zoom

---
+++ cardboard demo/Assets/Cardboard/Scripts/MouseDrag.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MouseDrag : MonoBehaviour, IDragHandler {
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		Debug.Log ("OnBeginDrag");
+		this.transform.position = eventData.position;
+	}
+
+	#endregion
+}
---
+++ cardboard demo/Assets/Cardboard/Scripts/VRDevices/LookAtCameraYonly.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCameraYonly : MonoBehaviour {
+
+	public Camera cameraToLookAt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 v = cameraToLookAt.transform.position - transform.position;
+		//v.x = v.z = 0.0f;
+		//transform.LookAt( cameraToLookAt.transform.position - v ); 
+		transform.rotation.Set(0,cameraToLookAt.transform.rotation.y,0,0);
+	}
+}
