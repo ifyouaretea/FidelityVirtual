a281a0f68d9d7b467e9d244a6a79774a48c342c2
taykangsheng <kangsheng@gmail.com>
2016-04-16 17:16:50 +0800

    sublime editor
    added self made sublime text… bit ugly… but can work.

---
+++ cardboard demo/Assets/sublime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class sublime : MonoBehaviour {
+
+	public Text textbox;
+	public InputField inputfield;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textbox.text = inputfield.text;
+	}
+}
